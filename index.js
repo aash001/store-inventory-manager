65fe621dbde3222df4c1d96e54707f7f97b55435
aash001 <ampinarioash@users.noreply.github.com>
2022-06-16 10:08:33 -0400

    Add solution and styling

--- index.js
+++ index.js
@@ -1,41 +1,52 @@
-const inventoryDisplayTable = document.querySelector("tbody")
-const submitButton = document.querySelector(".button")
-const newItemForm = document.querySelector("form")
-const nextButton = document.querySelector("#next")
-const previousButton = document.querySelector("#previous")
+const inventoryDisplayTable = document.querySelector("tbody");
+const submitButton = document.querySelector("#submit");
+const newItemForm = document.querySelector("form");
+const nextButton = document.querySelector("#next");
+const previousButton = document.querySelector("#previous");
+const itemNameInput = document.querySelector("#item-name");
+const itemQualityInput = document.querySelector("#item-quality");
+const itemSellInInput = document.querySelector("#item-sell-in");
+const itemSellInLabel = document.querySelector("label:nth-child(5)");
+
+itemNameInput.addEventListener("input", event => {
+    if (itemNameInput.value.toLowerCase().includes("sulfuras")) {
+        itemQualityInput.value = 80;
+        itemQualityInput.max = 80;
+        itemQualityInput.min = 80;
+        itemSellInInput.required = false;
+        itemSellInInput.classList.add("hidden");
+        itemSellInLabel.classList.add("hidden");
+    } else {
+        itemSellInInput.classList.remove("hidden");
+        itemSellInLabel.classList.remove("hidden");
+        itemQualityInput.max = 50;
+        itemQualityInput.min = 0;
+    }
+})
 
 let itemInformation = {};
 let itemDisplayArray = [];
-
 newItemForm.addEventListener("submit", (event) => {
     event.preventDefault()
     const formData = new FormData(event.target)
+
     itemInformation = {
         itemName: formData.get("item-name"),
-        itemSellIn: +formData.get("sell-in"),
+        itemSellIn: +formData.get("item-sell-in"),
         itemQuality: +formData.get("item-quality"),
         itemCategory: getCategory(formData.get("item-name"))
     }
     itemDisplayArray.push(itemInformation);
-    console.log(itemDisplayArray)
 
-    const tableNewInventoryItem = document.createElement("tr");
-    tableNewInventoryItem.innerHTML = `
-    <td class="item-name">${itemInformation.itemName}</td>
-    <td class="item-sell-in">${itemInformation.itemSellIn}</td>
-    <td>${itemInformation.itemQuality}</td>
+    const newInventoryItem = document.createElement("tr");
+    newInventoryItem.innerHTML = `
+        <td class="item-name">${itemInformation.itemName}</td>
+        <td class="item-sell-in">${itemInformation.itemSellIn}</td>
+        <td>${itemInformation.itemQuality}</td>
     `
-    tableNewInventoryItem.classList.add("custom-inventory-item");
-    inventoryDisplayTable.append(tableNewInventoryItem);
+    newInventoryItem.classList.add("custom-inventory-item");
+    inventoryDisplayTable.append(newInventoryItem);
 })
-
-nextButton.addEventListener("click", () => {
-    specialQuality();
-    limitQuality();
-    sellIn();
-    sellOut();
-})
-
 
 function getCategory(itemName) {
     if (itemName.includes("Aged Brie")) {
@@ -46,57 +57,134 @@ function getCategory(itemName) {
         return "sulfuras"
     } else if (itemName.includes("Conjured")) {
         return "conjured"
-    } else {
+    } else
         return "none"
+}
+
+nextButton.addEventListener("click", () => {
+    nextDayQuality();
+    subtractValues();
+})
+
+function nextDayQuality() {
+    itemDisplayArray.forEach(object => {
+        if (object.itemCategory == "aged") {
+            let calculatedQuality = object.itemQuality + 1
+            object.itemQuality = checkQualityNumber(calculatedQuality)
+        } else if (object.itemCategory === "backstage" && object.itemSellIn > 10) {
+            let calculatedQuality = object.itemQuality + 1
+            object.itemQuality = checkQualityNumber(calculatedQuality);
+        } else if (object.itemCategory === "backstage" && object.itemSellIn <= 10 && object.itemSellIn > 5) {
+            let calculatedQuality = object.itemQuality + 2
+            object.itemQuality = checkQualityNumber(calculatedQuality);
+        } else if (object.itemCategory === "backstage" && object.itemSellIn <= 5 && object.itemSellIn > 0) {
+            let calculatedQuality = object.itemQuality + 3
+            object.itemQuality = checkQualityNumber(calculatedQuality);
+        } else if (object.itemCategory === "backstage" && object.itemSellIn <= 0) {
+            return object.itemQuality = 0
+        } else if (object.itemCategory === "conjured" && object.itemSellIn > 0) {
+            let calculatedQuality = object.itemQuality - 2
+            object.itemQuality = checkQualityNumber(calculatedQuality)
+            return calculatedQuality
+        } else if (object.itemCategory === "conjured") {
+            let calculatedQuality = object.itemQuality - 4
+            object.itemQuality = checkQualityNumber(calculatedQuality)
+            return calculatedQuality
+        } else if (object.itemCategory === "sulfuras") {
+            return object.itemQuality;
+        } else if (object.itemCategory === "none" && object.itemSellIn > 0) {
+            let calculatedQuality = object.itemQuality - 1
+            object.itemQuality = checkQualityNumber(calculatedQuality)
+        } else {
+            let calculatedQuality = object.itemQuality - 2
+            object.itemQuality = checkQualityNumber(calculatedQuality)
+        }
+    })
+}
+
+function subtractValues() {
+    itemDisplayArray.forEach(object => {
+        const newInventoryItem = document.querySelector(".custom-inventory-item");
+        if (object.itemCategory === "sulfuras") {
+            object.itemSellIn = object.itemSellIn
+        } else {
+            object.itemSellIn--;
+        }
+        newInventoryItem.innerHTML = `
+        <td class="item-name">${object.itemName}</td>
+        <td class="item-sell-in">${(object.itemSellIn)}</td>
+        <td>${object.itemQuality}</td>
+        `
+        inventoryDisplayTable.append(newInventoryItem);
+    })
+}
+
+
+
+function checkQualityNumber(calculatedQualityNumber) {
+    if (calculatedQualityNumber > 50) {
+        return 50
+    } else if (calculatedQualityNumber < 0) {
+        return 0
+    } else {
+        return calculatedQualityNumber
     }
 }
 
-function sellIn() {
+previousButton.addEventListener("click", () => {
+    previousDayQuality();
+    addValues();
+})
+
+function previousDayQuality() {
+    itemDisplayArray.forEach(object => {
+        if (object.itemCategory == "aged") {
+            let calculatedQuality = object.itemQuality - 1
+            object.itemQuality = checkQualityNumber(calculatedQuality)
+        } else if (object.itemCategory === "backstage" && object.itemSellIn > 10) {
+            let calculatedQuality = object.itemQuality - 1
+            object.itemQuality = checkQualityNumber(calculatedQuality);
+        } else if (object.itemCategory === "backstage" && object.itemSellIn <= 10 && object.itemSellIn > 5) {
+            let calculatedQuality = object.itemQuality - 2
+            object.itemQuality = checkQualityNumber(calculatedQuality);
+        } else if (object.itemCategory === "backstage" && object.itemSellIn <= 5 && object.itemSellIn > 0) {
+            let calculatedQuality = object.itemQuality - 3
+            object.itemQuality = checkQualityNumber(calculatedQuality);
+        } else if (object.itemCategory === "backstage" && object.itemSellIn <= 0) {
+            return object.itemQuality = 0
+        } else if (object.itemCategory === "conjured" && object.itemSellIn > 0) {
+            let calculatedQuality = object.itemQuality + 2
+            object.itemQuality = checkQualityNumber(calculatedQuality)
+            return calculatedQuality
+        } else if (object.itemCategory === "conjured") {
+            let calculatedQuality = object.itemQuality + 4
+            object.itemQuality = checkQualityNumber(calculatedQuality)
+            return calculatedQuality
+        } else if (object.itemCategory === "sulfuras") {
+            return object.itemQuality;
+        } else if (object.itemCategory === "none" && object.itemSellIn > 0) {
+            let calculatedQuality = object.itemQuality + 1
+            object.itemQuality = checkQualityNumber(calculatedQuality)
+        } else {
+            let calculatedQuality = object.itemQuality + 2
+            object.itemQuality = checkQualityNumber(calculatedQuality)
+        }
+    })
+}
+
+function addValues() {
     itemDisplayArray.forEach(object => {
         const newInventoryItem = document.querySelector(".custom-inventory-item");
+        if (object.itemCategory === "sulfuras") {
+            object.itemSellIn = object.itemSellIn
+        } else {
+            object.itemSellIn++;
+        }
         newInventoryItem.innerHTML = `
         <td class="item-name">${object.itemName}</td>
-        <td class="item-sell-in">${(object.itemSellIn - 1)}</td>
+        <td class="item-sell-in">${(object.itemSellIn)}</td>
         <td>${object.itemQuality}</td>
         `
-        console.log(itemDisplayArray)
-        object.itemSellIn--;
-        inventoryDisplayTable.append(newInventoryItem)
-    })
-}
-
-function specialQuality() {
-    itemDisplayArray.forEach(object => {
-        if (object.itemCategory === "aged") {
-            object.itemQuality = object.itemQuality + 3
-        } else if (object.itemCategory === "backstage") {
-            object.itemQuality = object.itemQuality + 1
-        } else if (object.itemCategory === "sulfuras") {
-            object.itemQuality = object.itemQuality
-        } else if (object.itemCategory === "conjured") {
-            object.itemQuality = object.itemQuality - 2
-        }
-    })
-}
-
-function sellOut() {
-    itemDisplayArray.forEach(object => {
-        if (object.itemSellIn <= 0 && object.itemCategory === "none") {
-            object.itemQuality = object.itemQuality - 2
-        } else {
-            object.itemQuality--
-        }
-    })
-}
-
-function limitQuality() {
-    itemDisplayArray.forEach(object => {
-        if (object.itemQuality >= 50) {
-            return object.itemQuality === 50
-        } else if (object.itemQuality <= 0) {
-            return object.itemQuality === 0
-        } else {
-            object.itemQuality = object.itemQuality
-        }
+        inventoryDisplayTable.append(newInventoryItem);
     })
 }
